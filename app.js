1f619459a8ff1cf57206ed1465664ac6ff541ed7
manuel lantieri <manuel.lantieri02@gmail.com>
2025-01-29 22:42:45 +0100

    feat middleweare errors

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRouter = require('./routers/posts');
+const postsRouter = require("./routers/posts");
 
 // Middleware per gestire JSON
 app.use(express.json());
 
 // Registrazione del router
-app.use('/posts', postsRouter);
+app.use("/posts", postsRouter);
+
+// Middleware per le rotte non trovate (404)
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Endpoint non trovato" });
+});
+
+// Middleware per la gestione degli errori
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    error: err.message || "Errore interno del server",
+  });
+});
 
 // Avvio del server
 const PORT = 3000;
